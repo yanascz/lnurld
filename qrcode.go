c642bf9e568cc3795e160d9ac755fca793092b84
yanas <yanas@yanas.cz>
2023-02-06 10:58:47 +0100

    Use lightning thumbnail in QR codes as a fallback

--- qrcode.go
+++ qrcode.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"github.com/skip2/go-qrcode"
 	"golang.org/x/image/draw"
 	"image"
@@ -9,44 +10,47 @@ import (
 	"image/png"
 )
 
+//go:embed files/lightning.png
+var lightningPngData []byte
+
 func encodeQrCode(content string, thumbnail *Thumbnail, size int) ([]byte, error) {
 	qrCode, err := qrcode.New(content, qrcode.Medium)
 	if err != nil {
 		return nil, err
 	}
 
-	qrCodeImage := qrCode.Image(size)
-
+	thumbnailData := lightningPngData
 	if thumbnail != nil {
-		thumbnailImage, _, err := image.Decode(bytes.NewReader(thumbnail.bytes))
-		if err != nil {
-			return nil, err
-		}
-
-		qrCodeBounds := qrCodeImage.Bounds()
-		thumbnailBounds := thumbnailImage.Bounds()
-
-		thumbnailSize := thumbnailBounds.Size()
-		thumbnailDestSize := qrCodeBounds.Size().Div(5)
-		if thumbnailSize.X < thumbnailSize.Y {
-			thumbnailDestSize.X = thumbnailSize.X * thumbnailDestSize.Y / thumbnailSize.Y
-		} else if thumbnailSize.X > thumbnailSize.Y {
-			thumbnailDestSize.Y = thumbnailSize.Y * thumbnailDestSize.X / thumbnailSize.X
-		}
-
-		thumbnailOffset := qrCodeBounds.Size().Sub(thumbnailDestSize).Div(2)
-		thumbnailDestRect := image.Rectangle{Min: thumbnailOffset, Max: thumbnailOffset.Add(thumbnailDestSize)}
-
-		rgbaImage := image.NewRGBA(qrCodeBounds)
-		draw.Draw(rgbaImage, qrCodeBounds, qrCodeImage, image.Point{}, draw.Over)
-		draw.CatmullRom.Scale(rgbaImage, thumbnailDestRect, thumbnailImage, thumbnailBounds, draw.Over, nil)
-
-		qrCodeImage = rgbaImage
+		thumbnailData = thumbnail.bytes
 	}
+
+	thumbnailImage, _, err := image.Decode(bytes.NewReader(thumbnailData))
+	if err != nil {
+		return nil, err
+	}
+
+	qrCodeImage := qrCode.Image(size)
+	qrCodeBounds := qrCodeImage.Bounds()
+	thumbnailBounds := thumbnailImage.Bounds()
+
+	thumbnailSize := thumbnailBounds.Size()
+	thumbnailDestSize := qrCodeBounds.Size().Div(5)
+	if thumbnailSize.X < thumbnailSize.Y {
+		thumbnailDestSize.X = thumbnailSize.X * thumbnailDestSize.Y / thumbnailSize.Y
+	} else if thumbnailSize.X > thumbnailSize.Y {
+		thumbnailDestSize.Y = thumbnailSize.Y * thumbnailDestSize.X / thumbnailSize.X
+	}
+
+	thumbnailOffset := qrCodeBounds.Size().Sub(thumbnailDestSize).Div(2)
+	thumbnailDestRect := image.Rectangle{Min: thumbnailOffset, Max: thumbnailOffset.Add(thumbnailDestSize)}
+
+	rgbaImage := image.NewRGBA(qrCodeBounds)
+	draw.Draw(rgbaImage, qrCodeBounds, qrCodeImage, image.Point{}, draw.Over)
+	draw.CatmullRom.Scale(rgbaImage, thumbnailDestRect, thumbnailImage, thumbnailBounds, draw.Over, nil)
 
 	var qrCodePngData bytes.Buffer
 	pngEncoder := png.Encoder{CompressionLevel: png.BestCompression}
-	err = pngEncoder.Encode(&qrCodePngData, qrCodeImage)
+	err = pngEncoder.Encode(&qrCodePngData, rgbaImage)
 	if err != nil {
 		return nil, err
 	}
